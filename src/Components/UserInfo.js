c468b11c32535742182fe59205b6f6eebfacd31d
marcialkim16@gmail.com <marcialkim16@gmail.com>
2023-12-29 22:04:09 +0800

    Update

---
+++ src/Components/UserInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/UserInfo.css';
+
+function UserInfo() {
+  return (
+    <div className="big">
+      <p className="big">Hello, World</p>
+    </div>
+  );
+}
+
+export default UserInfo;
